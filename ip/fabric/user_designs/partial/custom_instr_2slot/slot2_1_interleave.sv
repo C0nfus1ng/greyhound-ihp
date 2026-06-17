c78df21bcd62d88cb861fb6a80f801185d54e055
Stefan Huwar <stefan.huwar@gmail.com>
2026-06-17 15:26:46 +0200

    initial 2slot instruction

---
+++ ip/fabric/user_designs/partial/custom_instr_2slot/slot2_1_interleave.sv
@@ -0,0 +1,19 @@
+module slot2_1_interleave ();
+    // Slot IOs
+    logic [15:0] op0, op1, result;
+    (* keep *) slot2_1_wrapper wrapper (
+        .op0_o    (op0),
+        .op1_o    (op1),
+        .result_i (result),
+    );
+
+    assign result[7] = op0[4];
+    assign result[6] = op1[4];
+    assign result[5] = op0[5];
+    assign result[4] = op1[5];
+    assign result[3] = op0[6];
+    assign result[2] = op1[6];
+    assign result[1] = op0[7];
+    assign result[0] = op1[7];
+
+endmodule
