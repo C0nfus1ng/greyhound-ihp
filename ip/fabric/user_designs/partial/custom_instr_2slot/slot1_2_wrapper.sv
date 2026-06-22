4cd53e4b02a98e540e32e27c67747725013b8d5f
Stefan Huwar <stefan.huwar@gmail.com>
2026-06-22 18:43:45 +0200

    working config with interchangeable slots

--- ip/fabric/user_designs/partial/custom_instr_2slot/slot1_2_wrapper.sv
+++ ip/fabric/user_designs/partial/custom_instr_2slot/slot1_2_wrapper.sv
@@ -1,66 +1,23 @@
 module slot1_2_wrapper (
-    output logic [15:0] op0_o,
-    output logic [15:0] op1_o,
-    input  logic [15:0] result_i,
+    output logic [2:0] op0_o,
+    output logic [3:0] op1_o,
+    input  logic [3:0] result_i,
 );
-    (* keep, BEL="X6Y1.A" *) Slot1_2_slot_con_X6Y1 slot1_2_x6y1 (
-        .W2END0(op1_o[15]),
-        .W2END1(op1_o[14]),
-        .W2END2(op1_o[13]),
-        .W2END3(op1_o[12]),
-        .W2END4(op1_o[11]),
-        .W2END5(op1_o[10]),
-        .W2END6(op1_o[9]),
-        .W2END7(op1_o[8]),
-    );
     (* keep, BEL="X6Y2.A" *) Slot1_2_slot_con_X6Y2 slot1_2_x6y2 (
-        .W2END0(op1_o[7]),
-        .W2END1(op1_o[6]),
-        .W2END2(op1_o[5]),
-        .W2END3(op1_o[4]),
-        .W2END4(op1_o[3]),
-        .W2END5(op1_o[2]),
-        .W2END6(op1_o[1]),
-        .W2END7(op1_o[0]),
-    );
-    (* keep, BEL="X6Y5.A" *) Slot1_2_slot_con_X6Y5 slot1_2_x6y5 (
-        .E2BEG0(result_i[15]),
-        .E2BEG1(result_i[14]),
-        .E2BEG2(result_i[13]),
-        .E2BEG3(result_i[12]),
-        .E2BEG4(result_i[11]),
-        .E2BEG5(result_i[10]),
-        .E2BEG6(result_i[9]),
-        .E2BEG7(result_i[8]),
+        .W2END0(op1_o[3]),
+        .W2END1(op1_o[2]),
+        .W2END2(op1_o[1]),
+        .W2END3(op1_o[0]),
     );
     (* keep, BEL="X6Y6.A" *) Slot1_2_slot_con_X6Y6 slot1_2_x6y6 (
-        .E2BEG0(result_i[7]),
-        .E2BEG1(result_i[6]),
-        .E2BEG2(result_i[5]),
-        .E2BEG3(result_i[4]),
-        .E2BEG4(result_i[3]),
-        .E2BEG5(result_i[2]),
-        .E2BEG6(result_i[1]),
-        .E2BEG7(result_i[0]),
-    );
-    (* keep, BEL="X6Y9.A" *) Slot1_2_slot_con_X6Y9 slot1_2_x6y9 (
-        .W2END0(op0_o[15]),
-        .W2END1(op0_o[14]),
-        .W2END2(op0_o[13]),
-        .W2END3(op0_o[12]),
-        .W2END4(op0_o[11]),
-        .W2END5(op0_o[10]),
-        .W2END6(op0_o[9]),
-        .W2END7(op0_o[8]),
+        .E2BEG0(result_i[3]),
+        .E2BEG1(result_i[2]),
+        .E2BEG2(result_i[1]),
+        .E2BEG3(result_i[0]),
     );
     (* keep, BEL="X6Y10.A" *) Slot1_2_slot_con_X6Y10 slot1_2_x6y10 (
-        .W2END0(op0_o[7]),
-        .W2END1(op0_o[6]),
-        .W2END2(op0_o[5]),
-        .W2END3(op0_o[4]),
-        .W2END4(op0_o[3]),
-        .W2END5(op0_o[2]),
-        .W2END6(op0_o[1]),
-        .W2END7(op0_o[0]),
+        .W2END1(op0_o[2]),
+        .W2END2(op0_o[1]),
+        .W2END3(op0_o[0]),
     );
 endmodule
--- ip/fabric/user_designs/partial/custom_instr_2slot/static_slot.sv
+++ ip/fabric/user_designs/partial/custom_instr_2slot/static_slot.sv
@@ -12,6 +12,14 @@ module static_slot();
     logic clk;
     (* keep *) Global_Clock clk_i (.CLK(clk));
 
+    // RST
+    logic rst;
+    WARMBOOT_wrapper WARMBOOT_wrapper (
+        .SLOT   (4'd0),
+        .BOOT   (1'b0),
+        .RESET  (rst)
+    );
+
     logic        issue_ready_o;
     logic        issue_accept_o;
     logic        issue_valid_i;
@@ -41,173 +49,111 @@ module static_slot();
     );
 
     // Slot BEL placement
-    logic [15:0] slot1_result, slot2_result;
+    logic [3:0] slot1_result, slot2_result;
 
     slot1_wrapper slot1 (
-        .op0_i      (issue_op0_i[15:0]),
-        .op1_i      (issue_op1_i[15:0]),
+        .op0_i      (op0_d),
+        .op1_i      (op1_d),
         .result_o   (slot1_result),
     );
 
     slot2_wrapper slot2 (
-        .op0_i      (issue_op0_i[31:16]),
-        .op1_i      (issue_op1_i[31:16]),
+        .op0_i      (op0_d),
+        .op1_i      (op1_d),
         .result_o   (slot2_result),
     );
 
     // Slot selecting
     logic [6:0]  opcode;
     logic [4:0]  rd;
-    logic        issue_valid_q;
 
     assign opcode = issue_instr_i[6:0];
     assign rd     = issue_instr_i[11:7];
 
-    assign issue_accept_o = issue_valid_i && (opcode == INSTR1);
+    assign issue_accept_o = issue_valid_i && (opcode == INSTR3);
     assign issue_ready_o  = 1'b1;
 
-    always_comb begin : OutputStage
-        result_rd_o    = rd;
-        result_valid_o = issue_valid_q;
-        result_id_o    = issue_id_i;
-        result_o       = {slot2_result, slot1_result};
+    logic [2:0] op0_d;
+    logic [3:0] op1_d;
+    logic [3:0] id_d;
+    logic [3:0] rd_d;
+    logic issue_valid_d;
+
+    always_ff @(posedge clk) begin
+        if (rst) begin
+            issue_valid_d <= 1'b0;
+        end else begin
+            issue_valid_d <= issue_valid_i;
+
+            if (issue_valid_i) begin
+                id_d  <= issue_id_i;
+                rd_d  <= rd;
+                op0_d <= issue_op0_i[2:0];
+                op1_d <= issue_op1_i[3:0];
+            end
+        end
     end
 
-    always_ff @(posedge clk) begin : ValidBuffer
-        issue_valid_q <= issue_valid_i;
+    always_ff @(posedge clk) begin
+        if (rst) begin
+            result_valid_o  <= 1'b0;
+        end else begin
+            result_valid_o <= 1'b0;
+
+            if (issue_valid_d) begin
+                result_valid_o  <= 1'b1;
+                result_id_o     <= id_d;
+                result_rd_o     <= rd_d;
+                result_o        <= {24'h0, slot1_result, slot2_result};  
+            end
+        end
     end
 endmodule
 
 module slot1_wrapper (
-    input  logic [15:0] op0_i,
-    input  logic [15:0] op1_i,
-    output logic [15:0] result_o,
+    input  logic [2:0] op0_i,
+    input  logic [3:0] op1_i,
+    output logic [3:0] result_o,
 );
-    (* keep, BEL="X6Y1.A" *) Static_slot_con_X6Y1 slot1_x6y1 (
-        .W2END0(op1_i[15]),
-        .W2END1(op1_i[14]),
-        .W2END2(op1_i[13]),
-        .W2END3(op1_i[12]),
-        .W2END4(op1_i[11]),
-        .W2END5(op1_i[10]),
-        .W2END6(op1_i[9]),
-        .W2END7(op1_i[8]),
-    );
     (* keep, BEL="X6Y2.A" *) Static_slot_con_X6Y2 slot1_x6y2 (
-        .W2END0(op1_i[7]),
-        .W2END1(op1_i[6]),
-        .W2END2(op1_i[5]),
-        .W2END3(op1_i[4]),
-        .W2END4(op1_i[3]),
-        .W2END5(op1_i[2]),
-        .W2END6(op1_i[1]),
-        .W2END7(op1_i[0]),
-    );
-    (* keep, BEL="X6Y5.A" *) Static_slot_con_X6Y5 slot1_x6y5 (
-        .E2BEG0(result_o[15]),
-        .E2BEG1(result_o[14]),
-        .E2BEG2(result_o[13]),
-        .E2BEG3(result_o[12]),
-        .E2BEG4(result_o[11]),
-        .E2BEG5(result_o[10]),
-        .E2BEG6(result_o[9]),
-        .E2BEG7(result_o[8]),
+        .W2END0(op1_i[3]),
+        .W2END1(op1_i[2]),
+        .W2END2(op1_i[1]),
+        .W2END3(op1_i[0]),
     );
     (* keep, BEL="X6Y6.A" *) Static_slot_con_X6Y6 slot1_x6y6 (
-        .E2BEG0(result_o[7]),
-        .E2BEG1(result_o[6]),
-        .E2BEG2(result_o[5]),
-        .E2BEG3(result_o[4]),
-        .E2BEG4(result_o[3]),
-        .E2BEG5(result_o[2]),
-        .E2BEG6(result_o[1]),
-        .E2BEG7(result_o[0]),
-    );
-    (* keep, BEL="X6Y9.A" *) Static_slot_con_X6Y9 slot1_x6y9 (
-        .W2END0(op0_i[15]),
-        .W2END1(op0_i[14]),
-        .W2END2(op0_i[13]),
-        .W2END3(op0_i[12]),
-        .W2END4(op0_i[11]),
-        .W2END5(op0_i[10]),
-        .W2END6(op0_i[9]),
-        .W2END7(op0_i[8]),
+        .E2BEG0(result_o[3]),
+        .E2BEG1(result_o[2]),
+        .E2BEG2(result_o[1]),
+        .E2BEG3(result_o[0]),
     );
     (* keep, BEL="X6Y10.A" *) Static_slot_con_X6Y10 slot1_x6y10 (
-        .W2END0(op0_i[7]),
-        .W2END1(op0_i[6]),
-        .W2END2(op0_i[5]),
-        .W2END3(op0_i[4]),
-        .W2END4(op0_i[3]),
-        .W2END5(op0_i[2]),
-        .W2END6(op0_i[1]),
-        .W2END7(op0_i[0]),
+        .W2END1(op0_i[2]),
+        .W2END2(op0_i[1]),
+        .W2END3(op0_i[0]),
     );
 endmodule
 
 module slot2_wrapper (
-    input  logic [15:0] op0_i,
-    input  logic [15:0] op1_i,
-    output logic [15:0] result_o,
+    input  logic [2:0] op0_i,
+    input  logic [3:0] op1_i,
+    output logic [3:0] result_o,
 );
-    (* keep, BEL="X2Y1.A" *) Static_slot_con_X2Y1 slot2_x2y1 (
-        .W2END0(op1_i[15]),
-        .W2END1(op1_i[14]),
-        .W2END2(op1_i[13]),
-        .W2END3(op1_i[12]),
-        .W2END4(op1_i[11]),
-        .W2END5(op1_i[10]),
-        .W2END6(op1_i[9]),
-        .W2END7(op1_i[8]),
-    );
     (* keep, BEL="X2Y2.A" *) Static_slot_con_X2Y2 slot2_x2y2 (
-        .W2END0(op1_i[7]),
-        .W2END1(op1_i[6]),
-        .W2END2(op1_i[5]),
-        .W2END3(op1_i[4]),
-        .W2END4(op1_i[3]),
-        .W2END5(op1_i[2]),
-        .W2END6(op1_i[1]),
-        .W2END7(op1_i[0]),
-    );
-    (* keep, BEL="X2Y5.A" *) Static_slot_con_X2Y5 slot2_x2y5 (
-        .E2BEG0(result_o[15]),
-        .E2BEG1(result_o[14]),
-        .E2BEG2(result_o[13]),
-        .E2BEG3(result_o[12]),
-        .E2BEG4(result_o[11]),
-        .E2BEG5(result_o[10]),
-        .E2BEG6(result_o[9]),
-        .E2BEG7(result_o[8]),
+        .W2END0(op1_i[3]),
+        .W2END1(op1_i[2]),
+        .W2END2(op1_i[1]),
+        .W2END3(op1_i[0]),
     );
     (* keep, BEL="X2Y6.A" *) Static_slot_con_X2Y6 slot2_x2y6 (
-        .E2BEG0(result_o[7]),
-        .E2BEG1(result_o[6]),
-        .E2BEG2(result_o[5]),
-        .E2BEG3(result_o[4]),
-        .E2BEG4(result_o[3]),
-        .E2BEG5(result_o[2]),
-        .E2BEG6(result_o[1]),
-        .E2BEG7(result_o[0]),
-    );
-    (* keep, BEL="X2Y9.A" *) Static_slot_con_X2Y9 slot2_x2y9 (
-        .W2END0(op0_i[15]),
-        .W2END1(op0_i[14]),
-        .W2END2(op0_i[13]),
-        .W2END3(op0_i[12]),
-        .W2END4(op0_i[11]),
-        .W2END5(op0_i[10]),
-        .W2END6(op0_i[9]),
-        .W2END7(op0_i[8]),
+        .E2BEG0(result_o[3]),
+        .E2BEG1(result_o[2]),
+        .E2BEG2(result_o[1]),
+        .E2BEG3(result_o[0]),
     );
     (* keep, BEL="X2Y10.A" *) Static_slot_con_X2Y10 slot2_x2y10 (
-        .W2END0(op0_i[7]),
-        .W2END1(op0_i[6]),
-        .W2END2(op0_i[5]),
-        .W2END3(op0_i[4]),
-        .W2END4(op0_i[3]),
-        .W2END5(op0_i[2]),
-        .W2END6(op0_i[1]),
-        .W2END7(op0_i[0]),
+        .W2END1(op0_i[2]),
+        .W2END2(op0_i[1]),
+        .W2END3(op0_i[0]),
     );
 endmodule
